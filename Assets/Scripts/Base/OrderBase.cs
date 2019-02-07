d2ba372c159e78e6c21c6b72109a24350c64e5c4
cmgonzalez <cmgonzalez@blizzard.com>
2019-02-06 18:44:58 -0800

    Some more orders, added a beacon class because why not.

--- Assets/Scripts/Base/OrderBase.cs
+++ Assets/Scripts/Base/OrderBase.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class OrderBase : MonoBehaviour
 {
@@ -11,20 +13,79 @@ public class OrderBase : MonoBehaviour
         Move,
         Attack,
         Patrol,
+        Wait,
         Work
     }
-    public TypeEnum OrderType;
-    public GameObject Target;
+
+    [Serializable]
+    public class Order
+    {
+        public TypeEnum OrderType;
+        public GameObject Target;
+        public float Duration;
+
+    }
+
+    public List<Order> OrderList;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    void Reset()
+    {
         
     }
+
+    void ExecuteNextOrder()
+    {
+
+        var order = OrderList.First();
+        switch (order.OrderType)
+        {
+            case TypeEnum.Move:
+                break;
+            case TypeEnum.Attack:
+                break;
+            case TypeEnum.Patrol:
+                break;
+            case TypeEnum.Wait:
+                StartCoroutine("OrderWait", order.Duration);
+                break;
+            case TypeEnum.Work:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        OrderList.Remove(order);
+    }
+
+    IEnumerator OrderWait(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+    }
+
+    IEnumerator OrderMove(GameObject target)
+    {
+        // Think about this some more. Is this really the structure you want?
+
+    }
+    void NextObjective()
+    {
+
+    }
+
+
+
 }
