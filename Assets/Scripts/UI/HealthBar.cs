5ab4dc3ebb114415786f7356ddec582353c41d9c
cmgonzalez <cmgonzalez@blizzard.com>
2019-02-12 18:58:12 -0800

    UI work pt... whatever

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     private Vector2 pos = new Vector2(20,40);
-    private Vector2 size = new Vector2(60,20);
+    private Vector2 size = new Vector2(100,60);
     public Texture2D progressBarEmpty;
     public Texture2D progressBarFull;
     public float barDisplay = 0f;
@@ -25,14 +25,13 @@ public class HealthBar : MonoBehaviour
     }
     private void OnGUI()
     {
+        
         GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y));
-        GUI.Box(new Rect(0, 0, size.x, size.y), progressBarEmpty);
-
-        // draw the filled-in part:
-        GUI.BeginGroup(new Rect(0, 0, size.x * barDisplay, size.y));
-        GUI.Box(new Rect(0, 0, size.x, size.y), progressBarFull);
-        GUI.EndGroup();
-
+            GUI.DrawTexture(new Rect(0, 0, size.x, size.y), progressBarEmpty);
+            // draw the filled-in part:
+              GUI.BeginGroup(new Rect(0, 0, size.x * barDisplay, size.y));
+                  GUI.Box(new Rect(0, 0, size.x, size.y), progressBarFull);
+              GUI.EndGroup();
         GUI.EndGroup();
     }
     
